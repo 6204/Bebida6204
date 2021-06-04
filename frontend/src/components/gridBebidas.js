7388f4b80a063c1fc4d46cfa74d7c6f596f5f049
lu4nsds <luan.s9d7s@gmail.com>
2021-06-04 16:41:06 -0300

    Creting grid list

--- frontend/src/components/gridBebidas.js
+++ frontend/src/components/gridBebidas.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import {Grid, Box} from '@material-ui/core';
+import {Grid, Box, Button, Paper} from '@material-ui/core';
 import {LocalBar} from '@material-ui/icons'
 import Typography from '@material-ui/core/Typography';
 
@@ -23,6 +23,18 @@ const bebidas = [
     name: 'Quinta do Morgado',
     teor: '25%',
 },
+{
+  id: 4,
+  categoriaId: 3,
+  name: 'Black Label',
+  teor: '42%',
+},
+{
+  id: 5,
+  categoriaId: 3,
+  name: 'Red Label',
+  teor: '47%',
+},
 ]
 
 const useStyles = makeStyles((theme) => ({
@@ -44,35 +56,55 @@ const useStyles = makeStyles((theme) => ({
     maxWidth: '100%',
     maxHeight: '100%',
   },
+  grid: {
+    width: '100%',
+    backgroundColor: '#f7bb28',
+  },
+  paper: {
+    padding: theme.spacing(2),
+    margin: 'auto',
+    maxWidth: 500,
+    background: '#f7bb28',
+    color: '#282c34',
+
+  },
+  
 }));
 
 export default function ComplexGrid() {
   const classes = useStyles();
 
   return (
-    <Box p={6} style={{ paddingInline: 200}}>
+    <Box p={6} style={{marginInline: 50,}}>
           <Typography
            variant='h5'
-           style={{fontWeight: 600, color: '#f7bb28'}} 
+           style={{fontWeight: 600, color: '#f7bb28', marginBottom: 20,}} 
           >
             Filtros  
           </Typography>
-          <Grid container spacing={4}  >
+          <Grid container style={{justifyContent: 'center'}} spacing={6}  >
             {
                 bebidas.map((item, index) => (
-                    <Grid item lg={3} md={4} sm={6} xs={12}>
-                        <Box>
-                            <LocalBar/>
-                            {item.name}
-                        </Box>
-                        <Box>
-                            <LocalBar/>
-                            {item.name}
-                        </Box>
-                        <Box>
-                            <LocalBar/>
-                            {item.name}
-                        </Box>
+                    <Grid item  lg={3} md={4} sm={6} xs={12}>
+                        <Paper className={classes.paper}>
+                          <Box>
+                            <Typography
+                            variant='h5'
+                            style={{width: '75%', color: '#282c34', backgroundColor: '#ffffff'}} 
+                            >                              
+                              {item.name}
+                            </Typography>
+                            <Button/>                            
+                          </Box>
+                          <Box>
+                              <LocalBar/>
+                              {item.name}
+                          </Box>
+                          <Box>
+                              <LocalBar/>
+                              {item.name}
+                          </Box>
+                        </Paper>
                     </Grid>
                 ))
 
